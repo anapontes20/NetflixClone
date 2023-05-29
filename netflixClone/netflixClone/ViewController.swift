6bebf3e0c31a06dccf5e6dc831df0350bac18279
Ana Pontes <analopesps20@gmail.com>
2023-05-29 19:21:21 -0300

    feat - projeto criado

---
+++ netflixClone/netflixClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  netflixClone
+//
+//  Created by Ana Pontes on 29/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
